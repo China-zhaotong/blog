36778512207f83004c3584c2874b49f2edd01a5c
zhaotong <1427730623@qq.com>
2023-06-29 17:42:09 +0800

    提交

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,11 +22,11 @@ export default defineConfig({
     shikiConfig: {
       // Choose from Shiki's built-in themes (or add your own)
       // https://github.com/shikijs/shiki/blob/main/docs/themes.md
-      theme: 'github-light',
+      theme: 'vitesse-light',
       // Add custom languages
       // Note: Shiki has countless langs built-in, including .astro!
       // https://github.com/shikijs/shiki/blob/main/docs/languages.md
-      langs: ['javascript'],
+      // langs: ['javascript'],
       // Enable word wrap to prevent horizontal scrolling
       wrap: true,
     },
